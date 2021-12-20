6b8238e75df030f7dafe96d27da0dd246450da37
Shivaansh Agarwal <shivanshagrawal1997@gmail.com>
2021-12-20 11:07:06 +0530

    feat: Adding google fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      cursive: ["Sacramento", "cursive"],
+      poppins: ["Poppins", "sans-serif"],
+      permanentmarker: ["Permanent Marker", "cursive"],
+    },
     extend: {},
   },
   plugins: [],
