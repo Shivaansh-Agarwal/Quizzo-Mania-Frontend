0db857e17488b293585b9ce81b8a316132fc9dbb
Shivaansh Agarwal <shivanshagrawal1997@gmail.com>
2021-12-22 13:07:50 +0530

    style: Change font size of creator-name

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export function Footer({ minHeight, maxHeight }) {
       className="flex flex-col justify-center items-center bg-blue-100"
       style={{ minHeight, maxHeight }}
     >
-      <div className="font-cursive text-3xl sm:text-4xl">Shivaansh Agarwal</div>
+      <div className="font-cursive text-2xl sm:text-3xl">Shivaansh Agarwal</div>
       <SocialMediaBar />
     </footer>
   );
