9daa50a0847a7187973991eccfb0e5be5309ba65
Shivaansh Agarwal <shivanshagrawal1997@gmail.com>
2021-12-22 13:06:38 +0530

    style: Adjust heights of Main and Footer components

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ function App() {
   return (
     <div className="font-poppins h-screen flex flex-col justify-start">
       <Header minHeight="10%" maxHeight="10vh" />
-      <Main minHeight="78%" maxHeight="78vh" />
-      <Footer minHeight="12%" maxHeight="12vh" />
+      <Main minHeight="80%" maxHeight="80vh" />
+      <Footer minHeight="10%" maxHeight="10vh" />
     </div>
   );
 }
